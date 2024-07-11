9925b2176a5c08b85c0e3bf86c5bc700cc8f52cf
syf3r9k <120647976+syf3r9k@users.noreply.github.com>
2024-07-12 00:16:45 +0300

    init

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# ConsoleMusic
- Console music player!
+# CLI Music Player
+
+This is my first project in Rust!
+
+This program is a CLI music player, utilizing the `{rodio, serde}` crates.
+
+## Features (Current Version)
+
+- Listen to music
+- Play/Stop music
+- Set music position
+- Set music
+- Save last played music
+
+## Future Enhancements
+
+- Add playlists
+- Add mpk (music package)
+- Install songs from different platforms and immediately add them to a playlist
+- Convert video (mp4) to music (mp3) with easy playlist addition
+- Listen to music together, even with large groups
+    - If group listening is added, also add voice chat for singing together
+- Add speech recognition
+
+## Contact
+
+Do not judge strictly; this is my first version. If you want to help, I will be happy to hear from you. Please contact me at: happinesssyf3r9k@gmail.com
--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,84 @@
+// modification
 mod config;
-
-use std::fs::File;
-use std::io::{Read, Write};
-use serde::{Deserialize, Serialize};
-
 use std::io;
 use std::io::BufReader;
-use rodio::{Decoder, OutputStream, Sink};
+use std::fs::File;
+use std::io::{Read, Write};
+use serde::{Deserialize, Serialize}; // Deserialize Serialize lib
+use rodio::{Decoder, OutputStream, Sink}; // Decoder, OutputStream and Sink w/ rodio
 
 fn main() {
-    let mut music_path = config::get_last_music();
-    let (stream, stream_handle) = OutputStream::try_default().unwrap();
-    let sink = Sink::try_new(&stream_handle).unwrap();
-    let mut run: bool = true;
-    let mut music_run: bool = false;
-    let mut seek_position: Option<u64> = None;
+    let mut music_path = config::get_last_music(); // get last music
+    let (stream, stream_handle) = OutputStream::try_default().unwrap(); // create a thread for audio io
+    let sink = Sink::try_new(&stream_handle).unwrap(); // for control music
+    let mut run: bool = true; // run flag
+    let mut music_run: bool = false; // music play flag on/off
+    let mut seek_position: Option<u64> = None; // variable to store search position
+
     while run {
-        let mut command = String::new();
-        let _ = io::stdin().read_line(&mut command);
-        let command = command.trim();
+        // main program cycle
+        let mut command = String::new(); // var to store command
+        let _ = io::stdin().read_line(&mut command); // user input
+        let command = command.trim(); // delete spaces
         if command.starts_with("ref ") {
-            let new_music_path = command.trim_start_matches("ref ").trim();
-            sink.stop();
-            sink.clear();
-            music_run = false;
-            music_path = String::from(new_music_path);
-            println!("Music path set to: {}", music_path);
-            seek_position = None;
+            // processing the music path change command
+            let new_music_path = command.trim_start_matches("ref ").trim(); // take new music path
+            sink.stop(); // stop music
+            sink.clear(); // clear sink
+            music_run = false; // set music run to false /off
+            music_path = String::from(new_music_path); // update path to music
+            println!("Music path set to: {}", music_path); // print new path
+            seek_position = None; // rest search position
         } else if command.starts_with("ps ") {
-            let position_str = command.trim_start_matches("ps ").trim();
+            // processing search command
+            let position_str = command.trim_start_matches("ps ").trim(); // take position
             match position_str.parse::<u64>() {
+                // parse to u64
                 Ok(pos) => {
                     if music_run {
-                        let _ = sink.try_seek(std::time::Duration::from_secs(pos));
+                        // if music run
+                        let _ = sink.try_seek(std::time::Duration::from_secs(pos)); // try take search position
                     } else {
-                        seek_position = Some(pos);
+                        seek_position = Some(pos); // if music of, save position for future search
                     }
-                    println!("Seeking to {} seconds.", pos);
+                    println!("Seeking to {} seconds.", pos); // print info about search pos
                 },
                 Err(_) => {
-                    println!("Invalid seconds format.");
+                    println!("Invalid seconds format."); // except
                 }
             }
         } else if command == "play" {
+            // processing play command
             if !music_run {
-                let file = File::open(&music_path).unwrap();
-                let source = Decoder::new(BufReader::new(file)).unwrap();
-                sink.append(source);
+                // if music play
+                let file = File::open(&music_path).unwrap(); // open music file
+                let source = Decoder::new(BufReader::new(file)).unwrap(); // create decoder for read file
+                sink.append(source); // add sink source
                 if let Some(pos) = seek_position {
-                    let _ = sink.try_seek(std::time::Duration::from_secs(pos));
-                    seek_position = None;
+                    let _ = sink.try_seek(std::time::Duration::from_secs(pos)); // set search position
+                    seek_position = None; // reset search position
                 }
-                sink.play();
-                music_run = true;
+                sink.play(); // start play
+                music_run = true; // set music run flag to true /on
             }
         } else if command == "stop" {
+            // processing stop command
             if music_run {
-                sink.pause();
-                music_run = false;
+                // if music play
+                sink.pause(); // stop music
+                music_run = false; // set music run flag to false /off
             }
         } else if command == "!cs" {
-            let sec = sink.get_pos();
-            let sec = sec.as_secs();
-            println!("{}", sec);
+            // set music pos
+            let sec = sink.get_pos(); // take now pos
+            let sec = sec.as_secs(); // convert to sec
+            println!("{}", sec); // print now pos
         } else if command == "!exit" {
-            config::set_last_music(&music_path);
-            sink.stop();
-            music_run = false;
-            run = false;
+            // processing exit
+            config::set_last_music(&music_path); // save last music path to config //// config file config.json //// my lib config.rs
+            sink.stop(); // stop music
+            music_run = false; // set music run flag to false
+            run = false; // stop main cycle and off program
         }
     }
 }
